34fd5860924258a974f674d2e2ebcc36f67d5a5a
iftekharhasanrifat <iftekharhasanrifat@gmail.com>
2025-03-01 02:11:41 +0600

    did some update

--- routes/tradersRoute.js
+++ routes/tradersRoute.js
@@ -125,70 +125,151 @@ router.delete('/:id', async (request,response) => {
 //     }
 // })
 
-router.get('/remainingTaka/:truckNo/:month/:year', async (request, response) => {
-    try {
-        const { truckNo, month, year } = request.params;
-        const decodedTruckNo = decodeURIComponent(truckNo);
+// router.get('/remainingTaka/:truckNo/:month/:year', async (request, response) => {
+//     try {
+//         const { truckNo, month, year } = request.params;
+//         const decodedTruckNo = decodeURIComponent(truckNo);
 
-        const traders = await Trader.find({ truckNo: decodedTruckNo });
+//         const traders = await Trader.find({ truckNo: decodedTruckNo });
         
         
-        let sum = 0;
-        let tranSum = 0;
-        const filteredData = traders.filter(trader => {
-            const [traderYear, traderMonth, day] = trader.date.split('-');
-            if (traderMonth === month && traderYear === year) {
-                tranSum += trader.transportCost;
-                sum += trader.remainingTaka;
-                return true;
-            }
-            return false;
-        });
+//         let sum = 0;
+//         let tranSum = 0;
+//         const filteredData = traders.filter(trader => {
+//             const [traderYear, traderMonth, day] = trader.date.split('-');
+//             if (traderMonth === month && traderYear === year) {
+//                 tranSum += trader.transportCost;
+//                 sum += trader.remainingTaka;
+//                 return true;
+//             }
+//             return false;
+//         });
         
-        return response.status(200).send({
-            total: sum,
-            transportCost: tranSum,
-            data: filteredData
+//         return response.status(200).send({
+//             total: sum,
+//             transportCost: tranSum,
+//             data: filteredData
             
             
-        });
-    } catch (error) {
-        console.log(error);
-        return response.status(500).send({ error: 'Internal Server Error' });
-    }
-});
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         return response.status(500).send({ error: 'Internal Server Error' });
+//     }
+// });
 
 
 // route to get sum of remaining taka by the end of the month
 
-router.get('/remainingTaka/:month/:year', async (request,response) => {
-    try{
-        const {month,year} = request.params;
-        const traders = await Trader.find({});
+// router.get('/remainingTaka/:month/:year', async (request,response) => {
+//     try{
+//         const {month,year} = request.params;
+//         const traders = await Trader.find({});
+//         let sum = 0;
+//         let tranSum = 0;
+//         const filteredData = traders.filter(trader => {
+//             const [traderYear, traderMonth, day] = trader.date.split('-');
+//             if (traderMonth === month && traderYear === year) {
+//                 tranSum += trader.transportCost;
+//                 sum += trader.remainingTaka;
+//                 return true;
+//             }
+//             return false;
+//         });
+//         return response.status(200).send({
+//             total: sum,
+//             transportCost: tranSum,
+//             data: filteredData
+//         });
+//     }
+//     catch(error){
+//         console.log(error);
+//     }
+// })
+
+
+router.get('/remainingTaka/:truckNo/:startDate/:endDate', async (req, res) => {
+    try {
+        const { truckNo, startDate, endDate } = req.params;
+        
+        // Parse startDate and endDate from yyyy-mm-dd to Date objects
+        const [sYear, sMonth, sDay] = startDate.split('-');
+        const start = new Date(sYear, sMonth - 1, sDay);
+        
+        const [eYear, eMonth, eDay] = endDate.split('-');
+        const end = new Date(eYear, eMonth - 1, eDay);
+        
+        // Find traders with the given truck number.
+        const traders = await Trader.find({ truckNo: truckNo });
+        
         let sum = 0;
         let tranSum = 0;
+        
+        // Filter traders whose date falls between start and end (inclusive)
         const filteredData = traders.filter(trader => {
-            const [traderYear, traderMonth, day] = trader.date.split('-');
-            if (traderMonth === month && traderYear === year) {
-                tranSum += trader.transportCost;
+            // Assuming trader.date is in "yyyy-mm-dd" format
+            const [year, month, day] = trader.date.split('-');
+            const traderDate = new Date(year, month - 1, day);
+            
+            if (traderDate >= start && traderDate <= end) {
                 sum += trader.remainingTaka;
+                tranSum += trader.transportCost;
                 return true;
             }
             return false;
         });
-        return response.status(200).send({
+        
+        return res.status(200).send({
             total: sum,
             transportCost: tranSum,
             data: filteredData
         });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ error: 'Internal Server Error' });
     }
-    catch(error){
-        console.log(error);
+});
+
+
+
+// route to get sum of remaining taka by the end of the month
+router.get('/remainingTaka/:startDate/:endDate', async (req, res) => {
+    try {
+        const { startDate, endDate } = req.params;
+        
+        // Parse the start and end dates from dd/mm/yyyy to Date objects
+        const [sYear,sMonth , sDay] = startDate.split('-');
+        const start = new Date(sYear, sMonth - 1, sDay);
+        
+        const [eYear, eMonth, eDay] = endDate.split('-');
+        const end = new Date(eYear, eMonth - 1, eDay);
+        
+        // First, get all traders .
+        const traders = await Trader.find({ });
+        let sum = 0;
+        let tranSum = 0;
+        // Filter traders whose date falls between the start and end dates (inclusive)
+        const filteredData = traders.filter(trader => {
+            const [year, month, day] = trader.date.split('-');
+            const traderDate = new Date(year, month - 1, day);
+            if (traderDate >= start && traderDate <= end) {
+                sum += trader.remainingTaka;
+                tranSum += trader.transportCost;
+                return true;
+            }
+            return false;
+        });
+        
+        return res.status(200).send({
+            total: sum,
+            transportCost: tranSum,
+            data: filteredData
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ error: 'Internal Server Error' });
     }
-})
-
-
-
+});
 
 
 export default router;
